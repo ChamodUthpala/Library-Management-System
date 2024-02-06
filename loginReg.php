a5da827b8eb519e12acd3527bb5945281a85615c
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-06 20:05:56 +0530

    added display_users

--- loginReg.php
+++ loginReg.php
@@ -128,48 +128,51 @@ if (isset($_POST['submit']) && isset($_GET['edit_user_id'])) {
 }
 
 
+//display_users.php
+         
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "library_test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$result = $conn->query("SELECT * FROM user");
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["user_id"] . "</td>";
+        echo "<td>" . $row["first_name"] . "</td>";
+        echo "<td>" . $row["last_name"] . "</td>";
+        echo "<td>" . $row["username"] . "</td>";
+        echo "<td>" . $row["password"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+
+        echo "<td>";
+        echo "<button class='edit' onclick='editUser(\"$row[user_id]\", \"$row[first_name]\", \"$row[last_name]\", \"$row[username]\", \"$row[password]\",\"$row[email]\")'>Edit</button>&nbsp";
+        echo "<button class='delete' onclick='deleteUserr(\"" . $row['user_id'] . "\")'>Delete</button>";
+        echo "</td>";
+        echo "</tr>";
+    }
+}
+
+
 $conn->close();
 ?>
 
 
-<?php
-//display_users.php
-         
-         $servername = "localhost";
-         $username = "root";
-         $password = "";
-         $database = "library_test";
 
-         // Create connection
-         $conn = new mysqli($servername, $username, $password, $database);
-
-         // Check connection
-         if ($conn->connect_error) {
-             die("Connection failed: " . $conn->connect_error);
-         }
-        
-
-
-         $result = $conn->query("SELECT * FROM user");
-
-         if ($result->num_rows > 0) {
-             while ($row = $result->fetch_assoc()) {
-                 echo "<tr>";
-                 echo "<td>" . $row["user_id"] . "</td>";
-                 echo "<td>" . $row["first_name"] . "</td>";
-                 echo "<td>" . $row["last_name"] . "</td>";
-                 echo "<td>" . $row["username"] . "</td>";
-                 echo "<td>" . $row["password"] . "</td>";
-                 echo "<td>" . $row["email"] . "</td>";
-
-                 echo "<td>";
-                 echo "<button class='edit' onclick='editUser(\"$row[user_id]\", \"$row[first_name]\", \"$row[last_name]\", \"$row[username]\", \"$row[password]\",\"$row[email]\")'>Edit</button>&nbsp";
-                 echo "<button class='delete' onclick='deleteUserr(\"" . $row['user_id'] . "\")'>Delete</button>";
-                 echo "</td>";
-                 echo "</tr>";
-             }
-         }
+ 
          
 
 
-         ?>
+         
